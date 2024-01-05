535fed4f831b688ccf6c10cfb93e83b01d4b3db5
tanmaypatidar29 <96785905+tanmaypatidar29@users.noreply.github.com>
2024-01-05 13:13:17 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentSymbol = s[i];
+        const currentValue = romanValues[currentSymbol];
+        if (i < s.length - 1) {
+            const nextSymbol = s[i + 1];
+            const nextValue = romanValues[nextSymbol];
+            if (currentValue < nextValue) {
+                result += (nextValue - currentValue);
+                i++; 
+                continue;
+            }
+        }
+        result += currentValue;
+    }
+
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
