5f8986a07d1c385e4e42342d27e9824889b2a97b
tanmaypatidar29 <96785905+tanmaypatidar29@users.noreply.github.com>
2024-01-05 12:59:55 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,18 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let mapping = {')': '(', '}': '{', ']': '['};
+    for (let char of s) {
+        if (char in mapping) {
+            let topElement = stack.pop() || '#';
+            if (mapping[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 };
-
 module.exports = { isValid };
-
-
